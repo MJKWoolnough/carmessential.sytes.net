98c42ca12d4683dcd4f6886b1ab3cb4b6aa52901
Michael Woolnough <michael.woolnough@gmail.com>
2017-10-14 12:07:07 +0100

    simplified

--- database.go
+++ database.go
@@ -3,97 +3,22 @@ package main
 import (
 	"database/sql"
 	"sync"
-	"time"
 
 	_ "github.com/mxk/go-sqlite/sqlite3"
 )
 
-var db database
-
-const (
-	DBCreateUser = iota
-	DBCreateTreatmentGroup
-	DBCreateTreatment
-	DBCreateProduct
-	DBCreateOrder
-	DBCreateVoucher
-	DBCreateBooking
-	DBCreateOrderedProduct
-	DBTotalStatements
-)
-
-type database struct {
-	mu sync.Mutex
-	db *sql.DB
+var db struct {
+	sync.Mutex
+	*sql.DB
 }
 
-func (d *database) init(filename string) error {
+func dbInit(filename string) error {
 	var err error
-	if d.db, err = sql.Open("sqlite3", filename); err != nil {
+	if db.DB, err = sql.Open("sqlite3", filename); err != nil {
 		return err
 	}
-	if err = users.init(d); err != nil {
+	if err = users.init(); err != nil {
 		return err
 	}
-	return nil
-}
-
-func (d *database) CreateTreatment(t *Treatment) error {
-	return nil
-}
-
-func (d *database) UpdateTreatment(t *Treatment) error {
-	return nil
-}
-
-func (d *database) GetTreatment(t *Treatment) error {
-	return nil
-}
-
-func (d *database) GetTreatments(tid int) ([]Treatment, error) {
-	return nil, nil
-}
-
-func (d *database) CreateProduct(p *Product) error {
-	return nil
-}
-
-func (d *database) UpdateProduct(p *Product) error {
-	return nil
-}
-
-func (d *database) GetProduct(p *Product) error {
-	return nil
-}
-
-func (d *database) GetProducts(pid int) ([]Product, error) {
-	return nil, nil
-}
-
-func (d *database) CreateBooking(b *Booking) error {
-	return nil
-}
-
-func (d *database) GetBooking(b *Booking) error {
-	return nil
-}
-
-func (d *database) UpdateBooking(b *Booking) error {
-	return nil
-}
-
-func (d *database) GetBookings(from, to time.Time) ([]Booking, error) {
-	return nil, nil
-}
-
-func (d *database) CreateVoucher(v *Voucher) error {
-	return nil
-}
-
-func (d *database) GetVoucher(v *Voucher) error {
-	return nil
-}
-
-func (d *database) UseVoucher(v *Voucher) error {
 	return nil
 }
--- user.go
+++ user.go
@@ -5,6 +5,8 @@ import (
 	"crypto/sha256"
 	"database/sql"
 	"math/rand"
+
+	"github.com/MJKWoolnough/errors"
 )
 
 const saltLength = 16
@@ -12,13 +14,13 @@ const saltLength = 16
 var users Users
 
 type Users struct {
-	*database
 	userID, userHash, createUser, updatePassword, updateEmail, getUserName *sql.Stmt
 }
 
-func (u *Users) init(db *database) error {
-	u.database = db
-	_, err := db.db.Exec("CREATE TABLE IF NOT EXIST [User]([ID] INTEGER PRIMARY KEY AUTOINCREMENT, [Name] TEXT NOT NULL, [EmailAddress] TEXT NOT NULL, [Password] BLOB NOT NULL);")
+func (u *Users) init() error {
+	db.Lock()
+	defer db.Unlock()
+	_, err := db.Exec("CREATE TABLE IF NOT EXIST [User]([ID] INTEGER PRIMARY KEY AUTOINCREMENT, [Name] TEXT NOT NULL, [EmailAddress] TEXT NOT NULL, [Password] BLOB NOT NULL);")
 	if err != nil {
 		return err
 	}
@@ -28,20 +30,19 @@ func (u *Users) init(db *database) error {
 		&u.createUser:     "INSERT INTO [User]([Name], [EmailAddress], [Password]) VALUES (?, ?, ?);",
 		&u.updatePassword: "UPDATE [User] SET [Password] = ? WHERE [ID] = ?;",
 		&u.updateEmail:    "UPDATE [User] SET [EmailAddress] = ? WHERE [ID] = ?;",
-		&getUserName:      "SELECT [Name] FROM [User] WHERE [ID] = ?;",
+		&u.getUserName:    "SELECT [Name] FROM [User] WHERE [ID] = ?;",
 	} {
-		*stmt, err = db.db.Prepare(query)
+		*stmt, err = db.Prepare(query)
 		if err != nil {
 			return err
 		}
 	}
-	u.mu = &db.mu
 	return nil
 }
 
 func passwordHash(salt []byte, password string) []byte {
 	hash := sha256.Sum256(append(salt, password...))
-	return append(salt, hash[:])
+	return append(salt, hash[:]...)
 }
 
 func passwordBuffer(password string) []byte {
@@ -54,9 +55,9 @@ func passwordBuffer(password string) []byte {
 
 func (u *Users) UserID(emailAddress string) (int64, error) {
 	var id int64
-	u.mu.Lock()
+	db.Lock()
 	err := u.userID.QueryRow(emailAddress).Scan(&id)
-	u.mu.Unlock()
+	db.Unlock()
 	if err != nil {
 		return 0, err
 	}
@@ -69,10 +70,10 @@ func (u *Users) UserHash(id int64) (sql.RawBytes, error) {
 	return passHash, err
 }
 
-func comparePassword(id int64, password string, saltedHash []byte) error {
+func comparePassword(password string, saltedHash []byte) error {
 	pass := passwordBuffer(password)
 	copy(pass, saltedHash)
-	if !bytes.Compare(saltedHash, passwordHash(append(pass, password...))) {
+	if !bytes.Equal(saltedHash, passwordHash(pass, password)) {
 		return ErrInvalidPassword
 	}
 	return nil
@@ -86,12 +87,12 @@ func (u *Users) CreateUser(name, emailAddress, password string) (int64, error) {
 	saltedHash := passwordHash(salt, password)
 
 	var id int64
-	u.mu.Lock()
+	db.Lock()
 	res, err := u.createUser.Exec(name, emailAddress, saltedHash)
 	if err == nil {
 		id, err = res.LastInsertId()
 	}
-	u.mu.Unlock()
+	db.Unlock()
 	if err != nil {
 		return 0, err
 	}
@@ -99,41 +100,54 @@ func (u *Users) CreateUser(name, emailAddress, password string) (int64, error) {
 }
 
 func (u *Users) LoginUser(id int64, password string) error {
-	u.mu.Lock()
-	err := comparePassword(id, password, u.userHash(id))
-	u.mu.Unlock()
+	db.Lock()
+	hash, err := u.UserHash(id)
+	if err == nil {
+		err = comparePassword(password, hash)
+	}
+	db.Unlock()
 	return err
 }
 
 func (u *Users) UpdateUserPassword(id int64, oldPassword, newPassword string) error {
-	u.mu.Lock()
-	saltedHash := d.userHash(id)
-	err := d.comparePassword(id, oldPassword, saltedHash)
+	db.Lock()
+	saltedHash, err := u.UserHash(id)
 	if err == nil {
-		saltedHash = passwordHash(saltedHash[:saltLength], newPassword)
-		err = u.updatePassword.Exec(saltedHash, id)
+		err = comparePassword(oldPassword, saltedHash)
+		if err == nil {
+			saltedHash = passwordHash(saltedHash[:saltLength], newPassword)
+			_, err = u.updatePassword.Exec(saltedHash, id)
+		}
 	}
-	u.mu.Unlock()
+	db.Unlock()
 	return err
 }
 
-func (d *database) UpdateUserEmail(id int64, emailAddress, password string) error {
-	u.mu.Lock()
-	err := comparePassword(id, password, u.userHash(id))
+func (u *Users) UpdateUserEmail(id int64, emailAddress, password string) error {
+	db.Lock()
+	saltedHash, err := u.UserHash(id)
 	if err == nil {
-		err = u.updateEmail.Exec(emailAddress, &id)
+		err = comparePassword(password, saltedHash)
+		if err == nil {
+			_, err = u.updateEmail.Exec(emailAddress, &id)
+		}
 	}
-	u.mu.Unlock()
+	db.Unlock()
 	return err
 }
 
-func (d *database) GetUserName(id int) (string, error) {
+func (u *Users) GetUserName(id int) (string, error) {
 	var username string
-	u.mu.Lock()
+	db.Lock()
 	err := u.getUserName.QueryRow(id).Scan(&username)
-	u.mu.Unlock()
+	db.Unlock()
 	if err != nil {
 		return "", err
 	}
 	return username, nil
 }
+
+// Errors
+var (
+	ErrInvalidPassword = errors.Error("invalid password")
+)
