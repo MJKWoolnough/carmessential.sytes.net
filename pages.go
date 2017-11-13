1032bb6b60bf5d8ee2a2657a91aae84ae7df7714
Michael Woolnough <michael.woolnough@gmail.com>
2017-11-13 19:40:19 +0000

    added template loading code

--- pages.go
+++ pages.go
@@ -1,15 +1,57 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+	"os"
+)
 
 var Pages pages
 
 type pages struct {
-	header, loggedIn, loggedOut, preBasket, postBasket, footer []byte
+	header, loggedIn, loggedOut, preBasket, noBasket, postBasket, footer []byte
 }
 
-func (p *pages) init() {
+func (p *pages) init(header, loggedIn, loggedOut, preBasket, noBasket, postBasket, footer string) error {
+	var err error
+	if p.header, err = loadFile(header); err != nil {
+		return err
+	}
+	if p.header, err = loadFile(loggedIn); err != nil {
+		return err
+	}
+	if p.header, err = loadFile(loggedOut); err != nil {
+		return err
+	}
+	if p.header, err = loadFile(preBasket); err != nil {
+		return err
+	}
+	if p.header, err = loadFile(noBasket); err != nil {
+		return err
+	}
+	if p.header, err = loadFile(postBasket); err != nil {
+		return err
+	}
+	if p.header, err = loadFile(footer); err != nil {
+		return err
+	}
+}
 
+func loadFile(filename string) ([]byte, error) {
+	fi, err := os.Stat(filename)
+	if err != nil {
+		return nil, err
+	}
+	buf := make([]byte, 0, fi.Size())
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	_, err := io.ReadFull(f, buf)
+	if err != nil {
+		return nil, err
+	}
+	return buf, f.Close()
 }
 
 type Page struct {
