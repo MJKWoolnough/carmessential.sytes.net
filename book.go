2a20c3a598140ca860988b7c6202a4241f037a20
Michael Woolnough <michael.woolnough@gmail.com>
2018-04-10 13:34:31 +0100

    loaded booking data

--- book.go
+++ book.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"net/http"
+	"sort"
 	"time"
 
 	"github.com/MJKWoolnough/errors"
@@ -16,8 +17,11 @@ type bookings struct {
 }
 
 type Booking struct {
-	ID   uint
-	Time time.Time
+	ID          uint
+	Time        time.Time
+	Treatment   uint
+	User, Order uint
+	Name        string
 }
 
 type bookingList []Booking
@@ -35,9 +39,32 @@ func (b bookingList) Swap(i, j int) {
 }
 
 func (b *bookings) Init(db *sql.DB) error {
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS [Booking]([ID] INTEGER PRIMARY KEY AUTOINCREMENT, [ID] INTEGER NOT NULL, [Treatment] INTEGER NOT NULL, [User] INTEGER NOT NULL DEFAULT 0, [Name] STRING NOT NULL);")
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS [Booking]([ID] INTEGER PRIMARY KEY AUTOINCREMENT, [Time] INTEGER NOT NULL, [Treatment] INTEGER NOT NULL, [User] INTEGER NOT NULL DEFAULT 0, [Order] INTEGER NOT NULL DEFAULY 0, [Name] STRING NOT NULL);")
 	if err != nil {
 		return errors.WithContext("error creating Booking table: ", err)
+	}
+	brows, err := db.Query("SELECT [ID], [Time], [Treatment], [User], [Order], [Name] FROM [Booking];")
+	if err != nikl {
+		return errors.WithContext("error retrieving booking data: ", err)
+	}
+
+	for brows.Next() {
+		var (
+			booking Booking
+			t       uint64
+		)
+		err = brows.Scan(&booking.ID, &t, &booking.Treatment, &booking.User, &booking.Order, &booking.Name)
+		if err != nil {
+			return errors.WithContext("error reading booking row: ", err)
+		}
+		booking.Time = time.Unix(t, 0)
+		y, m, d := booking.Time.Date()
+		date := uint(y)*10000 + uint(m)*100 + uint(d)
+		b.bookings[date] = append(b.bookings[date], booking)
+	}
+
+	for _, bookings := range b.bookings {
+		sort.Sort(bookings)
 	}
 
 	if err = Pages.RegisterTemplate("booking.tmpl"); err != nil {
