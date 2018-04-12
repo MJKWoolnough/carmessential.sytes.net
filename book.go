59d9a7db419716a4ba1db750c4eb45e6f27dc2c3
Michael Woolnough <michael.woolnough@gmail.com>
2018-04-12 14:13:37 +0100

    start of booking page code

--- book.go
+++ book.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"net/http"
 	"sort"
+	"strconv"
 	"time"
 
 	"github.com/MJKWoolnough/errors"
@@ -38,6 +40,11 @@ func (b bookingList) Swap(i, j int) {
 	b[i], b[j] = b[j], b[i]
 }
 
+func timeToDate(t time.Time) uint {
+	y, m, d := time.Date()
+	return uint(y)*10000 + uint(m)*100 + uint(d)
+}
+
 func (b *bookings) Init(db *sql.DB) error {
 	_, err := db.Exec("CREATE TABLE IF NOT EXISTS [Booking]([ID] INTEGER PRIMARY KEY AUTOINCREMENT, [Time] INTEGER NOT NULL, [Treatment] INTEGER NOT NULL, [User] INTEGER NOT NULL DEFAULT 0, [Order] INTEGER NOT NULL DEFAULY 0, [Name] STRING NOT NULL);")
 	if err != nil {
@@ -58,8 +65,7 @@ func (b *bookings) Init(db *sql.DB) error {
 			return errors.WithContext("error reading booking row: ", err)
 		}
 		booking.Time = time.Unix(t, 0)
-		y, m, d := booking.Time.Date()
-		date := uint(y)*10000 + uint(m)*100 + uint(d)
+		date := timeToDate(booking.Time)
 		b.bookings[date] = append(b.bookings[date], booking)
 	}
 
@@ -73,6 +79,52 @@ func (b *bookings) Init(db *sql.DB) error {
 	return nil
 }
 
-func (b *bookings) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+type DayData struct {
+	Date     time.Time
+	Bookings bookingList
+}
 
+func (b *bookings) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	treatmentID, _ := strconv.ParseUint(r.PostForm.Get("id"), 10, 32)
+	treatment, exists := Treatments.GetTreatment(uint(treatmentID))
+	if !exists {
+		http.Redirect(w, r, "/")
+		return
+	}
+	if timeStr := r.PostForm.Get("time"); timeStr != "" {
+		timeSec, err := strconv.ParseUint(timeStr, 10, 64)
+		if err != nil {
+			http.Error(w, "error", http.StatusNotAcceptable)
+			return
+		}
+		// validate time
+		// write confirmation page & book
+	}
+	page, _ := strconv.ParseUint(r.PostForm.Get("page"), 10, 32)
+	today := time.Now()
+	days := make([]DayData, 7)
+	for i := 0; i < 7; i++ {
+		day := today.Add(time.Hour * 24 * time.Duration(uint64(i)+page*7))
+		days[i] = DayData{
+			Date:     day,
+			Bookings: b.bookings[timeToDate(day)],
+		}
+	}
+	uid := Session.GetLogin(r)
+	basket := Session.LoadBasket(r)
+	r = r.WithContext(context.WithValue(context.WithValue(r.Context(), "userID", uid), "basket", basket))
+	Pages.Write(w, r, "booking.tmpl", struct {
+		Admin bool
+		*Basket
+		Page uint
+		Treatment
+		DayData
+	}{
+		Users.IsAdmin(uid),
+		basket,
+		page,
+		treatment,
+		days,
+	})
 }
