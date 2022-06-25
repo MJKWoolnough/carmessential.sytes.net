d95ad806dbb40a497fd74bb3400cbdb700526d19
Michael Woolnough <michael.woolnough@gmail.com>
2022-06-25 10:27:34 +0100

    receive header/footer

--- internal/static/rpc.ts
+++ internal/static/rpc.ts
@@ -3,12 +3,19 @@ import {RPC} from './lib/rpc.js';
 
 declare const pageLoad: Promise<void>;
 
+export let header = "",
+footer = "";
+
 export const rpc = {} as {
 },
 ready = pageLoad.then(() => WS("/admin")).then(ws => {
 	const arpc = new RPC(ws);
-	return arpc.await(-1).then(() => {
-		Object.freeze(Object.assign(rpc, {
-		}));
+	return arpc.await(-2).then(({header: h, footer: f}: {header: string, footer: string}) => {
+		header = h;
+		footer = f;
+		return arpc.await(-1).then(() => {
+			Object.freeze(Object.assign(rpc, {
+			}));
+		});
 	});
 });
