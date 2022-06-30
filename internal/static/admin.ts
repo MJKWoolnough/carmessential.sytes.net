0711120b27f03147f87b3823bffe0c690828aa6e
Michael Woolnough <michael.woolnough@gmail.com>
2022-06-30 14:41:35 +0100

    added basic UI for admin settings

--- internal/static/admin.ts
+++ internal/static/admin.ts
@@ -1,5 +1,6 @@
-import {clearNode} from './lib/dom.js';
-import {div, h1} from './lib/html.js';
+import {amendNode, clearNode} from './lib/dom.js';
+import {div, h1, li, ul} from './lib/html.js';
+import setHeaderFooter from './headfoot.js';
 import {body, ready} from './rpc.js';
 
 ready.catch(e => {
@@ -9,4 +10,11 @@ ready.catch(e => {
 	]);
 	throw e;
 }).then(() => {
+	const contents = div();
+	amendNode(body, [
+		ul([
+			li({"onclick": () => clearNode(contents, setHeaderFooter)}, "Set Header/Footer")
+		]),
+		contents
+	]);
 });
