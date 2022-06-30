8cfdce6472597cd623ee06aa802eaca37e9b6d0a
Michael Woolnough <michael.woolnough@gmail.com>
2022-06-30 14:57:40 +0100

    added code to check entered header + footer and send update to server

--- internal/static/headfoot.ts
+++ internal/static/headfoot.ts
@@ -1,10 +1,44 @@
 import {amendNode, clearNode} from './lib/dom.js';
 import {br, button, div, fieldset, legend, textarea} from './lib/html.js';
-import {footer, header, ready} from './rpc.js';
+import {footer, header, ready, rpc} from './rpc.js';
 import {labels} from './shared.js';
 
 const h = textarea(),
-      f = textarea();
+      f = textarea(),
+      update = button({"onclick": () => {
+	for (const e of elements) {
+		amendNode(e, {"disabled": true});
+	}
+	const wp = window.open("", "", "");
+	if (wp) {
+		wp.addEventListener("unload", () => {
+			for (const e of elements) {
+				amendNode(e, {"disabled": false});
+			}
+		});
+		const header = h.value,
+		      footer = f.value;
+		wp.document.documentElement.innerHTML = `${header}<div id="HEADERFOOTERTESTER"></div>${footer}`;
+		const tester = wp.document.getElementById("HEADERFOOTERTESTER");
+		if (tester) {
+			tester.replaceWith(button({"onclick": () => {
+				wp.close();
+				rpc.setHeaderFooter(header, footer);
+			}}, "Looks Good"), button({"onclick": () => wp.close()}, "Looks Bad"));
+		} else {
+			wp.close();
+			alert("Invalid Header or Footer");
+		}
+	} else {
+		alert("Need to allow pop-up windows");
+		return;
+	}
+      }}, "Update"),
+      clear = button({"onclick": () => {
+		clearNode(h, header);
+		clearNode(f, footer);
+      }}, "Reset"),
+      elements = [h, f, update, clear];
 
 ready.then(() => {
 	amendNode(h, header);
@@ -17,9 +51,6 @@ export default div(fieldset([
 	br(),
 	labels("Footer: ", f),
 	br(),
-	button({}, "Update"),
-	button({"onclick": () => {
-		clearNode(h, header);
-		clearNode(f, footer);
-	}}, "Reset")
+	update,
+	clear
 ]));
