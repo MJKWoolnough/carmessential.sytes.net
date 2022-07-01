2168594c83d29e029acd3c570c08d8836191689a
Michael Woolnough <michael.woolnough@gmail.com>
2022-07-01 11:54:43 +0100

    added treatment endpoints to RPCType

--- internal/static/types.ts
+++ internal/static/types.ts
@@ -1,3 +1,7 @@
 export type RPCType = {
 	setHeaderFooter: (header: string, footer: string) => Promise<void>;
+	listTreatments: () => Promise<[number, string, string, number, string, number][]>;
+	addTreatment: (name: string, group: string, price: number, description: string, duration: number) => Promise<number>;
+	setTreatment: (id: number, name: string, group: string, price: number, description: string, duration: number) => Promise<void>;
+	removeTreatment: (id: number) => Promise<void>;
 }
