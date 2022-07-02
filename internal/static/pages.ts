3ba6a2b9e16bad527da1edf81a21c3698a527e8f
Michael Woolnough <michael.woolnough@gmail.com>
2022-07-02 09:35:35 +0100

    added some styling to the menu

--- internal/static/pages.ts
+++ internal/static/pages.ts
@@ -1,5 +1,5 @@
 import {amendNode, clearNode} from './lib/dom.js';
-import {div, h1, li, ul} from './lib/html.js';
+import {div, h1, li, style, ul} from './lib/html.js';
 import {NodeMap, node, stringSort} from './lib/nodes.js';
 import {ready} from './rpc.js';
 
@@ -16,7 +16,7 @@ let currPage = "";
 
 const body = div(),
       section = div(),
-      pages = new NodeMap<string, Page>(ul(), (a: Page, b: Page) => stringSort(a.id, b.id));
+      pages = new NodeMap<string, Page>(ul({"id": "adminMenu"}), (a: Page, b: Page) => stringSort(a.id, b.id));
 
 export const setHeaderFooter = (h: string, f: string) => {
 	header = h;
@@ -50,6 +50,26 @@ ready.catch(e => {
 	}
 	throw e;
 }).then(() => {
+	amendNode(document.head, style({"type": "text/css"}, `
+#adminMenu {
+	list-style: none;
+	padding: 0;
+}
+
+#adminMenu > li {
+	background-color: #fff;
+	color: #000;
+	cursor: pointer;
+	display: inline-block;
+	padding: 0 1em;
+	text-align: center;
+}
+
+#adminMenu > li:hover {
+	background-color: #000;
+	color: #fff;
+}
+`));
 	amendNode(body, [
 		pages[node],
 		section
