b66613b4b734e843d4e765fdb70afd2656738b4a
Michael Woolnough <michael.woolnough@gmail.com>
2022-07-02 09:14:35 +0100

    all ready code now moved to pages module

--- internal/static/admin.ts
+++ internal/static/admin.ts
@@ -1,23 +1,3 @@
-import {amendNode, clearNode} from './lib/dom.js';
-import {div, h1, li, ul} from './lib/html.js';
-import setHeaderFooter from './headfoot.js';
-import {body} from './pages.js';
-import {ready} from './rpc.js';
-import treatments from './treatments.js';
+import './headfoot.js';
+import './treatments.js';
 
-ready.catch(e => {
-	clearNode(body, [
-		h1("Error"),
-		div(e.message ?? "Unknown Error")
-	]);
-	throw e;
-}).then(() => {
-	const contents = div();
-	amendNode(body, [
-		ul([
-			li({"onclick": () => clearNode(contents, setHeaderFooter)}, "Set Header/Footer"),
-			li({"onclick": () => clearNode(contents, treatments)}, "Edit Treatments")
-		]),
-		contents
-	]);
-});
