51caffd89f2308abca5576dce04811b01fda4ee1
Michael Woolnough <michael.woolnough@gmail.com>
2022-07-02 09:13:24 +0100

    pages can now be registered

--- internal/static/pages.ts
+++ internal/static/pages.ts
@@ -1,7 +1,22 @@
-import {div} from './lib/html.js';
+import {amendNode, clearNode} from './lib/dom.js';
+import {div, h1, li, ul} from './lib/html.js';
+import {NodeMap, node, stringSort} from './lib/nodes.js';
+import {ready} from './rpc.js';
+
+type Page = {
+	id: string;
+	fn?: () => Promise<void>;
+	[node]: HTMLLIElement;
+}
 
 export let header = "",
 footer = "";
+
+let currPage = "";
+
+const body = div(),
+      section = div(),
+      pages = new NodeMap<string, Page>(ul(), (a: Page, b: Page) => stringSort(a.id, b.id));
 
 export const setHeaderFooter = (h: string, f: string) => {
 	header = h;
@@ -10,4 +25,33 @@ export const setHeaderFooter = (h: string, f: string) => {
 	document.getElementById("ADMINBODY")!.replaceWith(body);
 	document.title = "Admin";
 },
-body = div();
+registerPage = (id: string, title: string, contents: HTMLElement, onchange?: () => Promise<void>) => {
+	pages.set(id, {
+		id,
+		fn: onchange,
+		[node]: li({"onclick": () => {
+			if (currPage !== id) {
+				(pages.get(currPage)?.fn?.() ?? Promise.resolve()).then(() => {
+					clearNode(section, contents);
+					currPage = id;
+				});
+			}
+		}}, title)
+	});
+};
+
+ready.catch(e => {
+	clearNode(body, [
+		h1("Error"),
+		div(e.message ?? "Unknown Error")
+	]);
+	if (!body.parentNode) {
+		clearNode(document.body, body);
+	}
+	throw e;
+}).then(() => {
+	amendNode(body, [
+		pages[node],
+		section
+	]);
+});
